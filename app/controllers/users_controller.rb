7e2e3e4dd9ba7426698d1c0ccfbdb3a5bab98d51
Takeshi_Hora <a1936th@aiit.ac.jp>
2019-05-14 11:34:51 +0000

    Finish ch08

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  include SessionsHelper
+
 
   def show
     @user = User.find(params[:id])
@@ -7,13 +9,14 @@ class UsersController < ApplicationController
 
   def new
     @user = User.new
-    #render "new"
+    render "new"
   end
   
   
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
 
