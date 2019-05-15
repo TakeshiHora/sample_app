ac4eefd4fcbab53a995a07880b04cdfcff2bbae9
Takeshi_Hora <a1936th@aiit.ac.jp>
2019-05-15 16:08:14 +0000

    Finish ch10

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,18 @@
 class UsersController < ApplicationController
-  include SessionsHelper
+  before_action :logged_in_user, only: [:index, :edit, :update,:destroy]
+  before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user,     only: [:destroy]
 
+  def index
+    @users = User.paginate(page: params[:page])
+  end
 
   def show
     @user = User.find(params[:id])
     #debugger
   end
 
+  #GET /users/new
   def new
     @user = User.new
     render "new"
@@ -19,16 +25,60 @@ class UsersController < ApplicationController
       log_in @user
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
-
     else
       render "new"
     end
   end
+  
+  #GET /users/:id/edit
+  #params[:id] => :id
+  def edit
+    @user = User.find(params[:id])
+    # => @user.errors.full_messages()
+  end
+  
+   #PATCH /users/:id
+  def update
+    @user = User.find(params[:id])
+    #@user.errors.full_messages()
+     if @user.update_attributes(user_params)
+      #success
+        flash[:success] = "Profile updated"
+        redirect_to @user
+     else
+      #Fail
+       render 'edit'
+     end
+  end
+  
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url
+  end
+   
   
   private
   
     def user_params
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
+    end
+  
+    def logged_in_user
+      unless logged_in?
+        store_location
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+    end
+    
+    def correct_user
+      @user = User.find(params[:id])
+       redirect_to(root_url) unless current_user?(@user)
+    end
+    
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
     end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -10,8 +10,23 @@ class ActiveSupport::TestCase
     !session[:user_id].nil?
   end
   
+    # テストユーザーとしてログインする
+  def log_in_as(user)
+    session[:user_id] = user.id
+  end
+  
   def log_out
     session.delete(:user_id)
     @current_user = nil
+  end
+end
+
+class ActionDispatch::IntegrationTest
+
+  # テストユーザーとしてログインする
+  def log_in_as(user, password: 'password', remember_me: '1')
+    post login_path, params: { session: { email: user.email,
+                                          password: password,
+                                          remember_me: remember_me } }
   end
 end
