8c091ba744a8f496b45697623f5ce8393c08a7b2
Takeshi_Hora <a1936th@aiit.ac.jp>
2019-05-26 06:38:35 +0000

    Finish ch11

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,8 +73,8 @@ Rails.application.configure do
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
     :authentication => :plain,
-    :user_name      => ENV['app130647375@heroku.com'],
-    :password       => ENV['omdg37wd5778'],
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
     :domain         => 'heroku.com',
     :enable_starttls_auto => true
   }
